d1aa69231edd6dcb579a59cbfeb0c4846c54302e
Diego Heredia <0229536@up.edu.mx>
2023-12-24 00:19:20 -0600

    CreatePost Done

--- src/lib/appwrite/api.ts
+++ src/lib/appwrite/api.ts
@@ -1,6 +1,6 @@
 import {ID, Query} from "appwrite"
-import { INewUser } from "@/types"
-import { account, appwriteConfig, avatars, database } from "./config"
+import { INewPost, INewUser } from "@/types"
+import { account, appwriteConfig, avatars, databases, storage } from "./config"
 
 export async function createUserAccount(user: INewUser) {
     try {
@@ -75,7 +75,7 @@ export async function getCurrentUser() {
         if(!currentAccount) throw new Error("No user found")
 
 
-        const currentUser = await database.listDocuments(
+        const currentUser = await databases.listDocuments(
             appwriteConfig.databaseId,
             appwriteConfig.userCollectionId,
             [Query.equal('accountId', currentAccount.$id)]
@@ -97,3 +97,88 @@ export async function signOutAccount() {
         console.log(error);        
     }
 }
+export async function createPost(post: INewPost) {
+    try {
+      // Upload file to appwrite storage
+      const uploadedFile = await uploadFile(post.file[0]);
+  
+      if (!uploadedFile) throw Error;
+  
+      // Get file url
+      const fileUrl = getFilePreview(uploadedFile.$id);
+      if (!fileUrl) {
+        await deleteFile(uploadedFile.$id);
+        throw Error;
+      }
+  
+      // Convert tags into array
+      const tags = post.tags?.replace(/ /g, "").split(",") || [];
+  
+      // Create post
+      const newPost = await databases.createDocument(
+        appwriteConfig.databaseId,
+        appwriteConfig.postCollectionId,
+        ID.unique(),
+        {
+          creator: post.userId,
+          caption: post.caption,
+          imageUrl: fileUrl,
+          imageId: uploadedFile.$id,
+          location: post.location,
+          tags: tags,
+        }
+      );
+  
+      if (!newPost) {
+        await deleteFile(uploadedFile.$id);
+        throw Error;
+      }
+  
+      return newPost;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  export async function uploadFile(file: File) {
+    try {
+      const uploadedFile = await storage.createFile(
+        appwriteConfig.storageId,
+        ID.unique(),
+        file
+      );
+  
+      return uploadedFile;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  // ============================== GET FILE URL
+export function getFilePreview(fileId: string) {
+    try {
+      const fileUrl = storage.getFilePreview(
+        appwriteConfig.storageId,
+        fileId,
+        2000,
+        2000,
+        "top",
+        100
+      );
+  
+      if (!fileUrl) throw Error;
+  
+      return fileUrl;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  
+  // ============================== DELETE FILE
+  export async function deleteFile(fileId: string) {
+    try {
+      await storage.deleteFile(appwriteConfig.storageId, fileId);
+  
+      return { status: "ok" };
+    } catch (error) {
+      console.log(error);
+    }
+  }
--- src/lib/appwrite/config.ts
+++ src/lib/appwrite/config.ts
@@ -4,6 +4,7 @@ export const appwriteConfig = {
     url: import.meta.env.VITE_APPWRITE_URL,
     projectId: import.meta.env.VITE_APPWRITE_PROJECT_ID,
     databaseId: import.meta.env.VITE_APPWRITE_DATABASE_ID,
+    storageId: import.meta.env.VITE_APPWRITE_STORAGE_ID,
     userCollectionId: import.meta.env.VITE_APPWRITE_USER_COLLECTION_ID,
     postCollectionId: import.meta.env.VITE_APPWRITE_POST_COLLECTION_ID,
     savesCollectionId: import.meta.env.VITE_APPWRITE_SAVES_COLLECTION_ID,
@@ -17,6 +18,6 @@ client
     .setProject(appwriteConfig.projectId)
 
 export const account = new Account(client)
-export const database = new Databases(client)
+export const databases = new Databases(client)
 export const storage = new Storage(client)
 export const avatars = new Avatars(client)
